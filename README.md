e477e23dd38317391579c8720b7a3140e252242a
Mahbubul-Haque <shohag7421@gmail.com>
2023-05-24 22:10:36 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gmsaiful71@gmail.com
+
+* +88 01712922516
+* +88 01511922516
+* gmsifulalam93@gmail.com
+* gmsaiful71@gmail.com
+* linkedin.com/in/saiful93/
+* github.com/SaifulAlam93
+* twitter.com/gmsifulalam93 # Fil-Upload-Using-Spring-boot
---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  private baseUrl = 'http://192.168.20.203:8080';
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    formData.append('image', file);
+
+    const req = new HttpRequest('POST', `${this.baseUrl}/image/fileSystem`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/image/fileSystem`);
+  }
+}
